4c04756a83c037b7f2269ab6e6a19d0aa628c7f2
Sebastian Wieland <wieland.s@mailbox.org>
2026-07-21 22:00:09 +0200

    fix: fixup msieve tap

--- Formula/msieve.rb
+++ Formula/msieve.rb
@@ -1,9 +1,9 @@
 class Msieve < Formula
   desc "Command-line client for direct ManageSieve protocol operations"
   homepage "https://github.com/SeWieland/Transiever.ManageSieve"
-  url "https://github.com/SeWieland/Transiever.ManageSieve/releases/download/v1.0.0/msieve-v1.0.0-linux-x64.tar.gz"
-  version "1.0.0"
-  sha256 "8716bf0c678348c3ec4a23e354e4068afbd3d28dc1475c477b2ac660e3d9dbbb"
+  url "https://github.com/SeWieland/Transiever.ManageSieve/releases/download/v1.0.1/msieve-v1.0.1-linux-x64.tar.gz"
+  version "1.0.1"
+  sha256 "47fe27ef079635225f645b20ae0608afba530ccfed7cf3c9b8787fcd8ba82d13"
   license "MIT"
 
   livecheck do
