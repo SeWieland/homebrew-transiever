19874f7bf2adb6ffe35f8431bf47d8731f5da381
SeWieland <26410655+SeWieland@users.noreply.github.com>
2026-08-29 21:39:25 +0200

    srtx 1.1.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/srtx.rb
+++ Formula/srtx.rb
@@ -1,8 +1,8 @@
 class Srtx < Formula
   desc "Create Sieve scripts from rule files; inspect, optimize, preview, and deploy"
   homepage "https://github.com/SeWieland/Transiever.SieveRuler"
-  url "https://github.com/SeWieland/Transiever.SieveRuler/releases/download/v1.1.3/srtx-v1.1.3-linux-x64.tar.gz"
-  sha256 "c6bcb081ad9699c583289a97f1fa9c6a7346ba01e8dca8341867a689cc0b16cd"
+  url "https://github.com/SeWieland/Transiever.SieveRuler/releases/download/v1.1.4/srtx-v1.1.4-linux-x64.tar.gz"
+  sha256 "b95bb2e8890957d31f82950f77ba319207b8eafb87be031b36df2a87fd15447c"
   license "MIT"
 
   livecheck do
