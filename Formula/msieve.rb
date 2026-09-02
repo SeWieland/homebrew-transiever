f4c190fa0887dbec5133d127546d896804fc1247
SeWieland <26410655+SeWieland@users.noreply.github.com>
2026-09-02 20:54:47 +0200

    msieve 1.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/msieve.rb
+++ Formula/msieve.rb
@@ -1,8 +1,8 @@
 class Msieve < Formula
   desc "ManageSieve client to list, download, upload, and activate Sieve scripts"
   homepage "https://github.com/SeWieland/Transiever.ManageSieve"
-  url "https://github.com/SeWieland/Transiever.ManageSieve/releases/download/v1.1.2/msieve-v1.1.2-linux-x64.tar.gz"
-  sha256 "8638dc79030fcf6d71f3b1240db927039f7e3a57c6b13b804210b647e9723975"
+  url "https://github.com/SeWieland/Transiever.ManageSieve/releases/download/v1.2.0/msieve-v1.2.0-linux-x64.tar.gz"
+  sha256 "a18e5d43ad537bddd7f2731614658c0bdb75d6388126596526820c2d46f22959"
   license "MIT"
 
   livecheck do
